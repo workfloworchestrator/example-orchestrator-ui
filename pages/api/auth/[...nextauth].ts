3a7b387a4d977bf90486a1e1fe7a1d890a5e1d09
Ricardo van der Heijden <20791917+ricardovdheijden@users.noreply.github.com>
2024-02-06 17:06:16 +0100

    460 Sets up oidc with a wellknown config. Adds OPA proof of concept

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,27 +2,39 @@ import NextAuth, { AuthOptions } from 'next-auth';
 import { JWT } from 'next-auth/jwt';
 import { OAuthConfig } from 'next-auth/providers';
 
-import { SessionWithToken } from '@orchestrator-ui/orchestrator-ui-components';
+import {
+    WfoSession,
+    WfoUserProfile,
+} from '@orchestrator-ui/orchestrator-ui-components';
 
 const token_endpoint_auth_method = process.env.NEXTAUTH_CLIENT_SECRET
     ? 'client_secret_basic'
     : 'none';
 
-export interface WfoProfile extends Record<string, string> {}
-
 const authActive = process.env.AUTH_ACTIVE?.toLowerCase() != 'false';
-const WfoProvider: OAuthConfig<WfoProfile> = {
+const wfoProvider: OAuthConfig<WfoUserProfile> = {
     id: process.env.NEXTAUTH_ID || '',
     name: process.env.NEXTAUTH_ID || '',
+    type: 'oauth',
     clientId: process.env.NEXTAUTH_CLIENT_ID || '',
     clientSecret: process.env.NEXTAUTH_CLIENT_SECRET || undefined,
-    wellKnown: `${
-        process.env.NEXTAUTH_ISSUER || ''
-    }/.well-known/openid-configuration`,
-    type: 'oauth',
+    wellKnown:
+        process.env.NEXTAUTH_WELL_KNOWN_OVERRIDE ??
+        `${process.env.NEXTAUTH_ISSUER || ''}/.well-known/openid-configuration`,
     authorization: { params: { scope: 'openid profile' } },
-    checks: ['pkce', 'state'],
     idToken: true,
+    checks: ['pkce', 'state'],
+    userinfo: {
+        request: async (context) => {
+            const { client, tokens } = context;
+
+            if (!context.provider.wellKnown || !tokens.access_token) {
+                return {};
+            }
+
+            return await client.userinfo(tokens.access_token);
+        },
+    },
     profile(profile) {
         return {
             id: profile.sub,
@@ -37,26 +49,23 @@ const WfoProvider: OAuthConfig<WfoProfile> = {
 };
 
 export const authOptions: AuthOptions = {
-    providers: authActive ? [WfoProvider] : [],
+    providers: authActive ? [wfoProvider] : [],
     callbacks: {
-        async jwt({ token, account }) {
-            // Persist the OAuth access_token to the token right after signin
+        async jwt({ token, account, profile }) {
+            // The "account" is only available right after signing in -- adding useful data to the token
             if (account) {
                 token.accessToken = account.access_token;
+                token.profile = profile;
             }
             return token;
         },
-        async session({
-            session,
-            token,
-        }: {
-            session: SessionWithToken;
-            token: JWT;
-        }) {
-            // Send properties to the client, like an access_token from a provider.
+        async session({ session, token }: { session: WfoSession; token: JWT }) {
+            // Assign data to the session to be available in the client through the useSession hook
+            session.profile = token.profile as WfoUserProfile | undefined;
             session.accessToken = token.accessToken
                 ? String(token.accessToken)
                 : '';
+
             return session;
         },
     },
