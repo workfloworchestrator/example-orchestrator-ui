1e5a13eecb642633963f6376660d8e9b5188e12c
Ricardo van der Heijden <20791917+ricardovdheijden@users.noreply.github.com>
2024-02-08 15:56:01 +0100

    460 Replaces useSessionWithToken and useSession with useWfoSession

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 
-import { useSession } from 'next-auth/react';
 import { useTranslations } from 'next-intl';
 
 import { EuiPageHeader, EuiSpacer } from '@elastic/eui';
-import { WfoStartPage } from '@orchestrator-ui/orchestrator-ui-components';
+import {
+    WfoStartPage,
+    useWfoSession,
+} from '@orchestrator-ui/orchestrator-ui-components';
 
 export function Index() {
-    const { data: session } = useSession();
+    const { session } = useWfoSession();
     const t = useTranslations('main');
     const username = session?.user?.name || '';
 
