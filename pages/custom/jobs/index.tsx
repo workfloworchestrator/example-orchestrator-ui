16b86fd2ad57d70f30395deeff947a225c7ac8d2
Ruben van Leeuwen <ruben.vanleeuwen@surf.nl>
2024-03-25 14:07:39 +0100

    Custom jobslist based on tasklist

---
+++ pages/custom/jobs/index.tsx
@@ -0,0 +1,198 @@
+import React, { useContext, useEffect, useState } from 'react';
+
+import { useTranslations } from 'next-intl';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { StringParam, useQueryParam, withDefault } from 'use-query-params';
+
+import {
+    EuiButton,
+    EuiFlexGroup,
+    EuiFlexItem,
+    EuiPageHeader,
+    EuiSpacer,
+} from '@elastic/eui';
+import {
+    ACTIVE_TASKS_LIST_TABLE_LOCAL_STORAGE_KEY,
+    COMPLETED_TASKS_LIST_TABLE_LOCAL_STORAGE_KEY,
+    DEFAULT_PAGE_SIZE,
+    StoredTableConfig,
+    WfoFilterTabs,
+    WfoIsAllowedToRender,
+    WfoStartTaskButtonComboBox,
+    WfoTableColumns,
+} from '@orchestrator-ui/orchestrator-ui-components';
+import { PATH_TASKS } from '@orchestrator-ui/orchestrator-ui-components';
+import {
+    ProcessListItem,
+    WfoProcessesList,
+} from '@orchestrator-ui/orchestrator-ui-components';
+import { PolicyResource } from '@orchestrator-ui/orchestrator-ui-components';
+import { ConfirmationDialogContext } from '@orchestrator-ui/orchestrator-ui-components';
+import {
+    useCheckEngineStatus,
+    useDataDisplayParams,
+    useMutateProcess,
+    useOrchestratorTheme,
+    useStoredTableConfig,
+} from '@orchestrator-ui/orchestrator-ui-components';
+import { WfoRefresh } from '@orchestrator-ui/orchestrator-ui-components';
+import {
+    WfoTasksListTabType,
+    defaultTasksListTabs,
+} from '@orchestrator-ui/orchestrator-ui-components';
+import { getTasksListTabTypeFromString } from '@orchestrator-ui/orchestrator-ui-components';
+import { SortOrder } from '@orchestrator-ui/orchestrator-ui-components';
+
+export default function JobsListPage() {
+    const router = useRouter();
+    const t = useTranslations('jobsList');
+    const [activeTab, setActiveTab] = useQueryParam(
+        'activeTab',
+        withDefault(StringParam, WfoTasksListTabType.ACTIVE),
+    );
+
+    const [tableDefaults, setTableDefaults] =
+        useState<StoredTableConfig<ProcessListItem>>();
+
+    const selectedTasksListTab = getTasksListTabTypeFromString(activeTab);
+
+    const localStorageKey =
+        selectedTasksListTab === WfoTasksListTabType.ACTIVE
+            ? ACTIVE_TASKS_LIST_TABLE_LOCAL_STORAGE_KEY
+            : COMPLETED_TASKS_LIST_TABLE_LOCAL_STORAGE_KEY;
+
+    const getStoredTableConfig =
+        useStoredTableConfig<ProcessListItem>(localStorageKey);
+
+    const { theme } = useOrchestratorTheme();
+    const { showConfirmDialog } = useContext(ConfirmationDialogContext);
+    const { retryAllProcesses } = useMutateProcess();
+    const { isEngineRunningNow } = useCheckEngineStatus();
+
+    useEffect(() => {
+        const storedConfig = getStoredTableConfig();
+
+        if (storedConfig) {
+            setTableDefaults(storedConfig);
+        }
+    }, [getStoredTableConfig]);
+
+    const { dataDisplayParams, setDataDisplayParam } =
+        useDataDisplayParams<ProcessListItem>({
+            // TODO: Improvement: A default pageSize value is set to avoid a graphql error when the query is executed
+            // https://github.com/workfloworchestrator/orchestrator-ui/issues/261
+            pageSize: tableDefaults?.selectedPageSize || DEFAULT_PAGE_SIZE,
+            sortBy: {
+                field: 'lastModifiedAt',
+                order: SortOrder.DESC,
+            },
+        });
+
+    const handleChangeTasksListTab = (
+        updatedTasksListTab: WfoTasksListTabType,
+    ) => {
+        setActiveTab(updatedTasksListTab);
+        setDataDisplayParam('pageIndex', 0);
+    };
+
+    const alwaysOnFilters = defaultTasksListTabs.find(
+        ({ id }) => id === selectedTasksListTab,
+    )?.alwaysOnFilters;
+
+    if (!selectedTasksListTab) {
+        router.replace(PATH_TASKS);
+        return null;
+    }
+
+    const handleRerunAllButtonClick = async () => {
+        if (await isEngineRunningNow()) {
+            showConfirmDialog({
+                question: t('rerunAllQuestion'),
+                confirmAction: () => {
+                    retryAllProcesses.mutate();
+                },
+            });
+        }
+    };
+
+    // Changing the order of the keys, resulting in an updated column order in the table
+    const handleOverrideTableColumns: (
+        defaultTableColumns: WfoTableColumns<ProcessListItem>,
+    ) => WfoTableColumns<ProcessListItem> = (defaultTableColumns) => ({
+        workflowName: {
+            field: 'workflowName',
+            name: t('jobName'),
+            render: (value, { processId }) => (
+                <Link href={`${PATH_TASKS}/${processId}`}>{value}</Link>
+            ),
+        },
+        lastStep: defaultTableColumns.lastStep,
+        lastStatus: defaultTableColumns.lastStatus,
+        workflowTarget: defaultTableColumns.workflowTarget,
+        productTag: defaultTableColumns.tag,
+        productName: defaultTableColumns.productName,
+        customer: defaultTableColumns.customer,
+        customerAbbreviation: defaultTableColumns.customerAbbreviation,
+        subscriptions: defaultTableColumns.subscriptions,
+        createdBy: defaultTableColumns.createdBy,
+        assignee: defaultTableColumns.assignee,
+        processId: defaultTableColumns.processId,
+        startedAt: defaultTableColumns.startedAt,
+        lastModifiedAt: defaultTableColumns.lastModifiedAt,
+    });
+
+    return (
+        <>
+            <EuiSpacer />
+
+            <EuiFlexGroup>
+                <EuiFlexItem>
+                    <EuiPageHeader pageTitle="Jobs"></EuiPageHeader>
+                </EuiFlexItem>
+
+                <EuiFlexItem>
+                    <EuiFlexGroup justifyContent="flexEnd">
+                        {' '}
+                        <WfoIsAllowedToRender
+                            resource={PolicyResource.TASKS_RETRY_ALL}
+                        >
+                            <EuiButton
+                                onClick={handleRerunAllButtonClick}
+                                iconType={() => (
+                                    <WfoRefresh
+                                        color={theme.colors.primaryText}
+                                    />
+                                )}
+                            >
+                                {t('rerunAll')}
+                            </EuiButton>
+                        </WfoIsAllowedToRender>
+                        <WfoIsAllowedToRender
+                            resource={PolicyResource.TASKS_CREATE}
+                        >
+                            <WfoStartTaskButtonComboBox />
+                        </WfoIsAllowedToRender>
+                    </EuiFlexGroup>
+                </EuiFlexItem>
+            </EuiFlexGroup>
+
+            <WfoFilterTabs
+                tabs={defaultTasksListTabs}
+                translationNamespace="tasks.tabs"
+                selectedTab={selectedTasksListTab}
+                onChangeTab={handleChangeTasksListTab}
+            />
+            <EuiSpacer size="xxl" />
+
+            <WfoProcessesList
+                defaultHiddenColumns={tableDefaults?.hiddenColumns}
+                localStorageKey={localStorageKey}
+                overrideDefaultTableColumns={handleOverrideTableColumns}
+                dataDisplayParams={dataDisplayParams}
+                setDataDisplayParam={setDataDisplayParam}
+                alwaysOnFilters={alwaysOnFilters}
+            />
+        </>
+    );
+}
