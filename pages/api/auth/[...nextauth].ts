2a47ad13ca04e13bcf7c02ed67671f775939cfdb
Mohammad Torkashvand <mohammad.torkashvand@geant.org>
2024-04-29 10:36:00 +0200

    rename NEXTAUTH_AUTHORIZATION_SCOPE to NEXTAUTH_AUTHORIZATION_SCOPE_OVERRIDE

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -23,7 +23,9 @@ const wfoProvider: OAuthConfig<WfoUserProfile> = {
         `${process.env.NEXTAUTH_ISSUER || ''}/.well-known/openid-configuration`,
     authorization: {
         params: {
-            scope: process.env.NEXTAUTH_AUTHORIZATION_SCOPE ?? 'openid profile',
+            scope:
+                process.env.NEXTAUTH_AUTHORIZATION_SCOPE_OVERRIDE ??
+                'openid profile',
         },
     },
     idToken: true,
