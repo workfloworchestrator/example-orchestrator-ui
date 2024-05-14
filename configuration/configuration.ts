114af9fcb5422c0d7f4658e688a4c4ae61a6e6f2
Ruben van Leeuwen <ruben.vanleeuwen@surf.nl>
2024-05-14 12:18:04 +0200

    1077: Prettier fixes

--- configuration/configuration.ts
+++ configuration/configuration.ts
@@ -11,7 +11,9 @@ export const DEFAULT_ORCHESTRATOR_API_BASE_URL = 'http://localhost:8080/api';
 export const DEFAULT_ORCHESTRATOR_WEBSOCKET_URL = 'ws://localhost:8080';
 
 export const ENGINE_STATUS_ENDPOINT = '/settings/status';
+export const PROCESS_STATUS_COUNTS_ENDPOINT = '/processes/status-counts';
 export const PROCESSES_ENDPOINT = '/processes';
+export const SUBSCRIPTION_ACTIONS_ENDPOINT = '/subscriptions/workflows';
 export const SUBSCRIPTION_PROCESSES_ENDPOINT =
     '/processes/process-subscriptions-by-subscription-id';
 export const DEFAULT_WORKFLOW_INFORMATION_LINK_URL = 'http://localhost:8080';
