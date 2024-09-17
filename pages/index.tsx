c845c5411f14da1b631351c9ffbd0cdceb3db1f8
Ricardo van der Heijden <20791917+ricardovdheijden@users.noreply.github.com>
2024-09-17 14:44:28 +0200

    1363 Removes header from app, it is part of the startpage in the library

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,9 @@
 import React from 'react';
 
-import { useTranslations } from 'next-intl';
-
-import { EuiPageHeader, EuiSpacer } from '@elastic/eui';
-import {
-    WfoStartPage,
-    useWfoSession,
-} from '@orchestrator-ui/orchestrator-ui-components';
+import { WfoStartPage } from '@orchestrator-ui/orchestrator-ui-components';
 
 export function Index() {
-    const { session } = useWfoSession();
-    const t = useTranslations('main');
-    const username = session?.user?.name || '';
-
-    return (
-        <>
-            <EuiPageHeader pageTitle={`${t('welcome')} ${username}`} />
-            <EuiSpacer />
-            <WfoStartPage />
-        </>
-    );
+    return <WfoStartPage />;
 }
 
 export default Index;
