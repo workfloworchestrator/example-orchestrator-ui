ed238a54e3a144f58855945e48113c05b11d67b1
Tim Frohlich <timfrohlich@hotmail.com>
2025-10-07 15:18:33 +0200

    Point to latest component package

--- pages/agent.tsx
+++ pages/agent.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 import { CopilotKit } from '@copilotkit/react-core';
 import '@copilotkit/react-ui/styles.css';
 import { WfoAgent } from '@orchestrator-ui/orchestrator-ui-components';
