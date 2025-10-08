09df96c0d47424377cab02610191ff7cb1dbf805
Ruben van Leeuwen <ruben.vanleeuwen@surf.nl>
2025-10-08 16:33:33 +0200

    Aligns packagelock

--- pages/agent.tsx
+++ pages/agent.tsx
@@ -1,8 +1,10 @@
+import React from 'react';
+
 import { CopilotKit } from '@copilotkit/react-core';
 import '@copilotkit/react-ui/styles.css';
 import { WfoAgent } from '@orchestrator-ui/orchestrator-ui-components';
 
-export default function SearchPage() {
+export default function AgentPage() {
     return (
         <CopilotKit runtimeUrl="/api/copilotkit" agent="query_agent">
             <WfoAgent />
