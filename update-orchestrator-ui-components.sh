28516bbe73bd826517e20cc9e214ffcaa20def96
Ruben van Leeuwen <ruben@sinin.nl>
2026-08-06 17:33:35 +0200

    88: Change auto merging to creating a pr

--- update-orchestrator-ui-components.sh
+++ update-orchestrator-ui-components.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 set -euo pipefail
-
-npm update @orchestrator-ui/orchestrator-ui-components @orchestrator-ui/eslint-config-custom @orchestrator-ui/jest-config @orchestrator-ui/tsconfig
+xznpm update @orchestrator-ui/orchestrator-ui-components @orchestrator-ui/eslint-config-custom @orchestrator-ui/jest-config @orchestrator-ui/tsconfig
