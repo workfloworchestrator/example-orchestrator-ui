868b79057f1f7512d7ba098f3d7c3bb680c97ca0
Ruben van Leeuwen <ruben@sinin.nl>
2026-08-24 11:15:50 +0200

    229: Fix typo in npm update shell script

--- update-orchestrator-ui-components.sh
+++ update-orchestrator-ui-components.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 set -euo pipefail
-xznpm update @orchestrator-ui/orchestrator-ui-components @orchestrator-ui/eslint-config-custom @orchestrator-ui/jest-config @orchestrator-ui/tsconfig
+npm update @orchestrator-ui/orchestrator-ui-components @orchestrator-ui/eslint-config-custom @orchestrator-ui/jest-config @orchestrator-ui/tsconfig
